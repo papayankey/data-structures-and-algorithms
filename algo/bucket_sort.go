2ecebac1fc825af993239dbf67f5d2d34e038567
Benneth Yankey <yankeybenneth@gmail.com>
2022-07-14 21:01:07 +0000

    updated helper methods visibility

--- algo/bucket_sort.go
+++ algo/bucket_sort.go
@@ -4,16 +4,16 @@ package algo
 // to distribute elements into buckets or bins.
 // This implementation stores node pointers as buckets.
 func BucketSort(s []int, n int) []int {
-	max := getMax(s, n)
+	max := GetMax(s, n)
 	bins := make([]*Node, max+1)
 
 	for i := 0; i < n; i++ {
-		insert(bins, s[i], i)
+		insert(bins, s[i])
 	}
 
 	for i, j := 0, 0; i < max+1; i++ {
 		for bins[i] != nil {
-			s[j] = remove(bins, i)
+			s[j] = Remove(bins, i)
 			j++
 		}
 	}
@@ -29,7 +29,7 @@ type Node struct {
 
 // remove deletes bucket and returns bucket value
 // Bucket index is replaced with nil or next bucket
-func remove(b []*Node, i int) int {
+func Remove(b []*Node, i int) int {
 	val := b[i].value
 	b[i] = b[i].next
 	return val
@@ -37,7 +37,7 @@ func remove(b []*Node, i int) int {
 
 // insert adds slice element as bucket in buckets
 // Each bucket index corresponds to slice element value
-func insert(bins []*Node, i, j int) {
+func insert(bins []*Node, i int) {
 	n := &Node{value: i}
 	if bins[i] == nil {
 		bins[i] = n
@@ -50,8 +50,8 @@ func insert(bins []*Node, i, j int) {
 	}
 }
 
-// getMax returns the maximum element in the slice
-func getMax(s []int, n int) int {
+// GetMax returns the maximum element in the slice
+func GetMax(s []int, n int) int {
 	max := s[0]
 	for i := 1; i < n; i++ {
 		if s[i] > max {
