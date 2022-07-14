bdeeca8d44e2971fb65bfda0ce7d61ffb66ec22f
Benneth Yankey <yankeybenneth@gmail.com>
2022-07-14 15:43:51 +0000

    finished bucket sort implementation and added tests

--- algo/bucket_sort.go
+++ algo/bucket_sort.go
@@ -1,41 +1,52 @@
 package algo
 
 // BucketSort is a distribution sort. The concept is
-// to distribute elements into buckets/bins.
-// In this implementation, bucket is a slice of node pointers.
+// to distribute elements into buckets or bins.
+// This implementation stores node pointers as buckets.
 func BucketSort(s []int, n int) []int {
 	max := getMax(s, n)
 	bins := make([]*Node, max+1)
-	for i := 0; i < max; i++ {
-		bins[i] = nil
+
+	for i := 0; i < n; i++ {
+		insert(bins, s[i], i)
 	}
-	for j := 0; j < n; j++ {
-		insert(bins, s, j)
-	}
+
 	for i, j := 0, 0; i < max+1; i++ {
 		for bins[i] != nil {
-			s[j] = bins[i].value
-			bins[i] = bins[i].next
+			s[j] = remove(bins, i)
 			j++
 		}
 	}
+
 	return s
 }
 
-// Node represents element to store in bucket
+// Node represents bucket or bin
 type Node struct {
 	value int
 	next  *Node
 }
 
-// insert adds slice elements as nodes and store in buckets
-//
-func insert(bins []*Node, s []int, j int) {
-	n := &Node{value: s[j]}
-	if bins[s[j]] == nil {
-		bins[s[j]] = n
+// remove deletes bucket and returns bucket value
+// Bucket index is replaced with nil or next bucket
+func remove(b []*Node, i int) int {
+	val := b[i].value
+	b[i] = b[i].next
+	return val
+}
+
+// insert adds slice element as bucket in buckets
+// Each bucket index corresponds to slice element value
+func insert(bins []*Node, i, j int) {
+	n := &Node{value: i}
+	if bins[i] == nil {
+		bins[i] = n
 	} else {
-		bins[s[j]].next = n
+		curr := bins[i]
+		for curr.next != nil {
+			curr = curr.next
+		}
+		curr.next = n
 	}
 }
 
